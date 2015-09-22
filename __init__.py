55a004a606d63fe54cfe1c8119bd7386fbcd4f42
Alexander Skiridomov <n1nj4z33@gmail.com>
2015-09-22 22:36:44 +0300

    Create __init__.py

---
+++ __init__.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+'''
+A python wrapper for the Proxmox 2.x API
+For more information see https://pve.proxmox.com/
+'''
+__author__ = 'Alexander Skiridomov'
+__contact__ = 'n1nj4z33@gmail.com'
+__date__ = '21.09.2015'
